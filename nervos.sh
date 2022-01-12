b053f7764a8bf7b766a749d6cde1eea716fef27c
Sabi <49737728+sabi@users.noreply.github.com>
2022-01-12 04:36:23 -0500

    added docker-compose to install

--- nervos.sh
+++ nervos.sh
@@ -29,7 +29,7 @@ function install_docker {
   curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   apt update
-  apt -y install docker-ce docker-ce-cli containerd.io
+  apt -y install docker-ce docker-ce-cli containerd.io docker-compose
 }
 
 function install_workspace_tools {
